35ae78fdd90ebb3e466809659614086f2e046500
Yichen Huang <yichen41@illinois.edu>
2023-10-14 00:28:17 -0500

    Added navbar logo and contact me button

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -1,8 +1,17 @@
 import './navbar.css';
+import logo from '../../assets/logo.png'
 
 const Navbar = () => {
   return (
-    <div>navbar</div>
+    <nav className="navbar">
+      <img src={logo} alt="logo" className="logo" />
+      <div className="desktopMenu">
+
+      </div>
+      <button className="desktopMenuBtn">
+        <img src="" alt="" className="desktopMenuing" />Contact Me
+      </button>
+    </nav>
   )
 }
 export default Navbar;
