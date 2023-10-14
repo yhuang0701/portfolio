290e837622101b5d7bef39bc9c52f7e7c0fd5864
Yichen Huang <yichen41@illinois.edu>
2023-10-14 00:52:55 -0500

    Added css effects on navbar section

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -1,15 +1,21 @@
 import './navbar.css';
 import logo from '../../assets/logo.png'
+import { Link } from 'react-scroll'
+import contactImg from '../../assets/contact.png'
 
 const Navbar = () => {
   return (
     <nav className="navbar">
       <img src={logo} alt="logo" className="logo" />
       <div className="desktopMenu">
-
+        <Link className='desktopMenuListItem'>Home</Link>
+        <Link className='desktopMenuListItem'>About</Link>
+        <Link className='desktopMenuListItem'>Portfolio</Link>
+        <Link className='desktopMenuListItem'>Clients</Link>
       </div>
       <button className="desktopMenuBtn">
-        <img src="" alt="" className="desktopMenuing" />Contact Me
+        <img src={contactImg} alt="contact img"
+          className="desktopMenuImg" />Contact Me
       </button>
     </nav>
   )
