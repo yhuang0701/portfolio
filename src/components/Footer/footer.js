4b08b84ea258fd151cd5a1c5a6df1a1c16a96192
Yichen Huang <yichen41@illinois.edu>
2023-10-14 18:51:19 -0500

    Added footer section

---
+++ src/components/Footer/footer.js
@@ -0,0 +1,10 @@
+import './footer.css'
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      Copyright &#169; 2023 Yichen Huang. All rights reserved.
+    </footer>
+  )
+}
+export default Footer
