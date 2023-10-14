47183023ea8d223c85f8c88f49d26fce8652c2a3
Yichen Huang <yichen41@illinois.edu>
2023-10-14 18:43:31 -0500

    Finished contact section w/ css effects

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -1,9 +1,28 @@
 import './contact.css';
+import FacebookIcon from '../../assets/facebook-icon.png';
+import InstagramIcon from '../../assets/instagram.png';
+import TwitterIcon from '../../assets/twitter.png';
+import YoutubeIcon from '../../assets/youtube.png';
 
 const Contact = () => {
   return (
     <section id="contactPage">
-
+      <div id="contact">
+        <h1 className="contactPageTitle">Contact Me</h1>
+        <span className="contaceDesc">Please fill out the form below to discuss any work opportunities.</span>
+        <form className="contactForm">
+          <input type="text" className="name" placeholder="Your Name" />
+          <input type="email" className="email" placeholder="Your Email" />
+          <textarea className="msg" name="message" rows="5" placeholder="Your Message"></textarea>
+          <button type="submit" value="Send" className="submitBtn">Submit</button>
+          <div className="links">
+            <img src={FacebookIcon} alt="Facebook Icon" className="link" />
+            <img src={TwitterIcon} alt="Twitter Icon" className="link" />
+            <img src={InstagramIcon} alt="Instagram Icon" className="link" />
+            <img src={YoutubeIcon} alt="Youtube Icon" className="link" />
+          </div>
+        </form>
+      </div>
     </section>
   )
 }
