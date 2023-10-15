8e53ebccc3215687fa6ef8060bfb03f4d9487382
Yichen Huang <yichen41@illinois.edu>
2023-10-14 20:06:01 -0500

    add emailjs package and update email submission functionalities

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -3,16 +3,33 @@ import FacebookIcon from '../../assets/facebook-icon.png';
 import InstagramIcon from '../../assets/instagram.png';
 import TwitterIcon from '../../assets/twitter.png';
 import YoutubeIcon from '../../assets/youtube.png';
+import emailjs from '@emailjs/browser';
+import { useRef } from 'react';
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_sk5tjgf', 'template_76sy09s', form.current, 'zBtj_FcNHrGWWsaD4')
+      .then((result) => {
+        console.log(result.text);
+        e.target.reset();
+        alert("Email Sent!")
+      }, (error) => {
+        console.log(error.text);
+      });
+  };
+
   return (
     <section id="contactPage">
       <div id="contact">
         <h1 className="contactPageTitle">Contact Me</h1>
         <span className="contaceDesc">Please fill out the form below to discuss any work opportunities.</span>
-        <form className="contactForm">
-          <input type="text" className="name" placeholder="Your Name" />
-          <input type="email" className="email" placeholder="Your Email" />
+        <form className="contactForm" ref={form} onSubmit={sendEmail}>
+          <input type="text" className="name" placeholder="Your Name" name="from_name" />
+          <input type="email" className="email" placeholder="Your Email" name="from_email" />
           <textarea className="msg" name="message" rows="5" placeholder="Your Message"></textarea>
           <button type="submit" value="Send" className="submitBtn">Submit</button>
           <div className="links">
